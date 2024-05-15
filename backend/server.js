b994cdfbf407f1ddd282fc2e8cd76e4e6d207840
nvivek42 <viveknarole100@gmail.com>
2024-05-15 13:03:53 +0530

    made few changes

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,28 @@ app.use(bodyParser.urlencoded())
 
 app.use(bodyParser.json())
 
-app.get('/:id',(request,response)=>{
+app.post('/:id',(request,response)=>{
     
     const { id } = request.params;
   
-    const statement = `select votingCentreName from votingCentre where constituencyId = ${id}`
+    const statement = `select votingCentreId,votingCentreName from votingCentre where constituencyId = ${id}`
     
     db.execute(statement,(error,data) =>{
         response.send(utils.createResult(error,data))
     })
     console.log("welcome chinna thala")
 })
+app.post('/votingCentre/:id',(request,response)=>{
+    const { id } = request.params;
+  
+    const statement = `select id,queueLength,expectedTime, lastUpdated  from currentstatus where votingCentreId = ${id}`
+    
+    db.execute(statement,(error,data) =>{
+        response.send(utils.createResult(error,data))
+    })
+    console.log("welcome chinna thala 2")
+})
+
 app.listen(3000,(req,resp)=>{
     console.log('server listening at port 3000')
 })
--- front end/script.js
+++ front end/script.js
@@ -7,23 +7,23 @@ const url = 'http://localhost:3000'
 //     });  
 // });  
 $(document).ready(function() {
+    const selectElement = $('#votingCentre');
     $('#constituency').on('change', function() {
         
     $.ajax({
         url: url + '/'+$('#constituency').val(),
-        method: 'get',
-        // data:{
-            
+        method: 'post',
+        // data:{    
         //      id: selectedConstituency
         // },
         success: (response) => {
-            console.log(response)
+            //console.log(response)
             if(response['status'] == 'success'){
-                 //console.log('welcome to my application: '+response['data'][0]['votingCentreName'])
-                 for (var i = 0; i < response['data'].length; i++) {
-                    console.log(response['data'][i] )   
-                     }
                 
+                selectElement.empty();
+                $.each(response['data'], function(index, optionData) {
+                    selectElement.append($('<option></option>').attr('value', optionData['votingCentreId']).text(optionData['votingCentreName']));
+                  });
             }
             else{
                 alert(response['error'])
@@ -34,38 +34,49 @@ $(document).ready(function() {
         },
     })
 })
+
 })
 
-// $(document).ready(function() {
-//     $("#constituency").change (function () {
-//         var selectedConstituency = $(this).children("option:selected").text();  
-//         $.ajax({
-//             url: url + '/',
-//             method: 'GET',
-//             data:{
-                 
-//             },
-//             success: (response) => {
-//                 console.log(response)
-//                 if(response['status'] == 'success')
-//                     alert('welcome to my ecommerce application')
-//                 else{
-//                     alert(response['error'])
-//                 }
-//             },
-//             error: (response) => {
-//                 console.log(response)
-//             },
-//         })
-//     });
-//   });
+$(document).ready(function() {
+$('#votingCentre').on('change', function() {
+    var optionSelected = $("option:selected", this);
+    console.log(optionSelected.val())
+    $.ajax({
+        url: url + '/votingCentre/'+ optionSelected.val(),
+        method: 'post',
+        // data:{    
+        //      id: selectedConstituency
+        // },
+        success: (response) => {
+            if(response['status'] == 'success'){
+                console.log("tasting success")
+                // selectElement.empty();
+                
+                    $('#ajaxTableBody').empty(); // Clear existing rows
+                
+                    $.each(response['data'], function(index, item) {
+                        console.log(item)
+                        const row = $('<tr>');
+                        row.append($('<td>').text(item.id));
+                        row.append($('<td>').text(item.queueLength));
+                        row.append($('<td>').text(item.expectedTime));
+                        row.append($('<td>').text(item.lastUpdated));
+                        $('#ajaxTableBody').append(row);
+                    });
+                
+                
+            }
+            else{
+                console.log(response['error'])
+            }
+        },
+        error: (response) => {
+            console.log(response)
+        },
+    })
+})
+})
 
-
-$(document).ready (function selectVotingCentre () {  
-    $("#votingCentre").change (function () {  
-        var selectedVotingCentre = $(this).children("option:selected").text();  
-    }); 
-});  
 
 function reset() {
     $("#constituency").val('select')
